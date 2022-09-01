998e126c203b438acffab45ba44627b8a709378e
albalawibbg <98052577+albalawibbg@users.noreply.github.com>
2022-09-01 11:10:02 +0300

    Add files via upload

--- Ntech_Customer_Invoice/__manifest__.py
+++ Ntech_Customer_Invoice/__manifest__.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
     'name': 'Ntech_Customer_Invoice',
     'version': '15.0',
     'category': 'Invoice',
     'description': """Ntech_Customer_Invoice""",
-    'depends': ['account'],
-    'data': ['views/customer_personal_views.xml'],
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'security/security.xml',
+        'views/account_views.xml',
+    ],
     'installable': True,
     'auto_install': False,
     'application': True,
