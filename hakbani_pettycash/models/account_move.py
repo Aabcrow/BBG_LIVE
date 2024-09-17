9c12b06a48653ba7f172c59ccfb1e1a4915041b0
mohamed2022103 <102139556+mohamed2022103@users.noreply.github.com>
2024-09-17 11:35:41 +0300

    Add files via upload

--- hakbani_pettycash/models/account_move.py
+++ hakbani_pettycash/models/account_move.py
@@ -15,35 +15,37 @@ class AccountMove(models.Model):
 
 
     def write(self, vals):
-        if vals.get('date'):
-            if datetime.strptime(str(vals.get('date')), '%Y-%m-%d') > datetime.today():
-                raise UserError(
-                        _('Please Select Valid Date .'))
-        if vals.get('ref') and not vals.get('partner_id') :
-            move = self.env['account.move'].search([('partner_id', '=', self.partner_id.id),('ref','=',vals.get('ref'))])
-            if move:
-                raise UserError(
-                        _('Can Use Same Ref With Same Partner.'))
-        elif vals.get('ref') and vals.get('partner_id'):
-            move = self.env['account.move'].search([('partner_id', '=', vals.get('partner_id')),('ref','=',vals.get('ref'))])
-            if move:
-                raise UserError(
-                        _('Can Use Same Ref With Same Partner.'))
+        if vals.get('move_type') in ['in_invoice', 'in_refund', 'out_invoice', 'out_refund']:
+            if vals.get('date'):
+                if datetime.strptime(str(vals.get('date')), '%Y-%m-%d') > datetime.today():
+                    raise UserError(
+                            _('Please Select Valid Date .'))
+            if vals.get('ref') and not vals.get('partner_id') :
+                move = self.env['account.move'].search([('partner_id', '=', self.partner_id.id),('ref','=',vals.get('ref'))])
+                if move:
+                    raise UserError(
+                            _('Can Use Same Ref With Same Partner.'))
+            elif vals.get('ref') and vals.get('partner_id'):
+                move = self.env['account.move'].search([('partner_id', '=', vals.get('partner_id')),('ref','=',vals.get('ref'))])
+                if move:
+                    raise UserError(
+                            _('Can Use Same Ref With Same Partner.'))
 
         res = super(AccountMove, self).write(vals)
         return res
     @api.model
     def create(self, vals):
-        if vals.get('date'):
-            if datetime.strptime(str(vals.get('date')), '%Y-%m-%d') > datetime.today():
-                raise UserError(
-                        _('Please Select Valid Date .'))
-        if vals.get('ref'):
-            move = self.env['account.move'].search(
-                [('partner_id', '=', vals.get('partner_id')), ('ref', '=', vals.get('ref'))])
-            if move:
-                raise UserError(
-                    _('Can Use Same Ref With Same Partner.'))
+        if vals.get('move_type') in ['in_invoice', 'in_refund', 'out_invoice', 'out_refund']:
+            if vals.get('date'):
+                if datetime.strptime(str(vals.get('date')), '%Y-%m-%d') > datetime.today():
+                    raise UserError(
+                            _('Please Select Valid Date .'))
+            if vals.get('ref'):
+                move = self.env['account.move'].search(
+                    [('partner_id', '=', vals.get('partner_id')), ('ref', '=', vals.get('ref'))])
+                if move:
+                    raise UserError(
+                        _('Can Use Same Ref With Same Partner.'))
         res = super(AccountMove, self).create(vals)
         return res
 
